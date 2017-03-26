12dfe0e2cceadfdae3cd75cf4bebb18ad0fd9133
daphne remali <remalidaphne@gmail.com>
2017-03-26 17:13:36 +0200

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
+const webpack = require('webpack');
 const path = require('path');
 
-
 module.exports = {
-  // the entry file for the bundle
-  entry: path.join(__dirname, '/client/src/index.jsx'),
-
-  // the bundle file we will get in the result
+  entry: path.join(__dirname, 'src', 'app-client.js'),
   output: {
-    path: path.join(__dirname, '/client/dist/js'),
-    filename: 'app.js',
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js'
   },
-
   module: {
-
-    // apply loaders to files that meet given conditions
     loaders: [{
-      test: /\.jsx?$/,
-      include: path.join(__dirname, '/client/src'),
-      loader: 'babel-loader',
+      test: path.join(__dirname, 'src'),
+      loader: ['babel-loader'],
       query: {
-        presets: ["react", "es2015"]
+        cacheDirectory: 'babel_cache',
+        presets: ['react', 'es2015']
       }
-    }],
+    }]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.DedupePlugin(), // supprime tous les fichiers dupliqués (modules importés dans plus d'un module).
+    new webpack.optimize.OccurenceOrderPlugin(), //aide à réduire la taille du fichier du paquet résultant.
+    new webpack.optimize.UglifyJsPlugin({ // minifie et obfusque le faisceau résultant en utilisant UglifyJs.
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    })
+  ]
 };
